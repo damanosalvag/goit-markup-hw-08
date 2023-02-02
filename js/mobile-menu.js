cf9347438c7596cfd650479bc3929fca1d72ca1d
Dairo Manosalva <38769711+damanosalvag@users.noreply.github.com>
2023-02-01 20:21:07 -0500

    modal burger finished and details

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,14 @@
 (() => {
-    let menuButton = document.getElementById("menu-button");
-    let menuList = document.getElementById("menu-list");
+    let OpenMenuBtn = document.getElementById("open-menu-btn");
+    let menuModal = document.getElementById("menu-modal");
+    let closeMenuBtn = document.getElementById("menu-burger-btn");
 
-    menuButton.addEventListener("click", function () {
-        menuList.classList.toggle("nav-main");
-    });
+
+    OpenMenuBtn.addEventListener("click", toggleMenu);
+    closeMenuBtn.addEventListener("click", toggleMenu);
+    
+    function toggleMenu () {
+        menuModal.classList.toggle("popUp-hidden");
+    }
+
 })();
